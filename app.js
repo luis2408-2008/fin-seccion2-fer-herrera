f2b0de40151eb818847f9cb7f974be87a1484ce2
Luis Montoya <luislmmc2408@gmail.com>
2025-01-13 12:53:23 -0500

    fin-seccion2-fer-herrera

---
+++ app.js
@@ -0,0 +1,24 @@
+
+
+let numberOfLine = 1
+
+function increaseLineNumber () {
+    numberOfLine = numberOfLine + 3
+    
+}
+
+console.log("Line", numberOfLine)
+
+increaseLineNumber();
+console.log("Line", numberOfLine)
+
+increaseLineNumber();
+
+console.log("Line", numberOfLine)
+
+increaseLineNumber();
+
+console.log("Line", numberOfLine) 
+ 
+
+console.log("hola, soy luis")
---
+++ sections/section-2.ts
@@ -0,0 +1,20 @@
+let numberOfLine = 1
+
+ export function printLineNumber () {
+    numberOfLine++;
+    
+}
+
+console.log("Linea #", numberOfLine)
+
+printLineNumber();
+
+console.log("Linea #", numberOfLine)
+
+printLineNumber();
+
+console.log("Linea #", numberOfLine)
+printLineNumber();
+
+console.log("Linea #", numberOfLine) 
+ 
