83ad1ded5f3ef662a3032686aeadc5b5b1d5383e
Luis Montoya <luislmmc2408@gmail.com>
2025-01-15 15:33:58 -0500

    agregamos-basi-types.ts

---
+++ sections/basi-types.ts
@@ -0,0 +1,30 @@
+
+ export let firstName: string = 'Luis'
+
+let lastName: string = 'Fernando'
+
+//console.log(firstName, lastName);
+
+let fullName: string = firstName +  ' ' + lastName
+
+console.log(fullName);
+
+
+let number1: number = 40;
+
+let number2: number = 100;
+
+console.log(number1 - number2); 
+
+
+let isActive: boolean = true;
+
+let isRunning: boolean = false;
+
+let hasKids: boolean = true
+
+console.log('isActive', isActive);
+
+console.log('isRunning', isRunning);
+
+console.log('hasKids', hasKids)
