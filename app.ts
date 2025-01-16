5da4ba84328e921e31d12158a5e5f237d0bee2c1
Luis Montoya <luislmmc2408@gmail.com>
2025-01-16 18:57:05 -0500

    proceso

--- app.ts
+++ app.ts
@@ -1,1 +1,16 @@
-console.log('hola mundo');
+
+
+
+
+//let firstName = 'juan';
+
+ import { addTwoNumbers } from "./helpers/math-helpers";
+
+ // greet(firstName);
+
+  export let total: string = addTwoNumbers (30, 20);
+
+ console.log('total:', total  );
+
+
+ 
---
+++ helpers/math-helpers.ts
@@ -0,0 +1,7 @@
+
+ export function addTwoNumbers (num1: number, num2: number ):string {
+
+    return 'hola mundo: ' + num1 + ' ' + num2;
+}
+
+ 
